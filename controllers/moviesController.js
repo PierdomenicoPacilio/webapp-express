a05c237549ca747cf8382923bdd073238915d9ad
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-05-11 20:31:40 +0200

    index and show controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,11 +2,39 @@ const connection = require('../data/db');
 
 
 function index(req, res) {
-    res.send('index movies');
+
+    const sql = 'SELECT * FROM movies;';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Database connection error"
+            });
+        };
+
+        res.json(results);
+    });
 };
 
 function show(req, res) {
-    res.send('show movies');
+
+    const { id } = req.params;
+
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Database connection error"
+            });
+        };
+        if (results.length === 0) {
+            return res.status(404).json({
+                errorMessage: "No records found"
+            });
+        };
+        res.json(results[0]);
+    });
 };
 
 module.exports = { index, show };
